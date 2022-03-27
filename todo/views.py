d405328e48b91671c5964050e860d588286abcdc
ryo0705s <inspire.sasaki15@gmail.com>
2022-03-28 00:43:24 +0900

    todoの詳細ページを作成

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Todo
 # from django.http import HttpResponse
-from django.views.generic import ListView  
+from django.views.generic import ListView, DetailView
 # Create your views here.
 class TodoList(ListView):
   model = Todo
   context_object_name = "tasks"
+class TodoDetail(DetailView):
+  model = Todo
+  context_object_name = "task"
 
 # def index(request):
 #   return HttpResponse("<h1> hello world</h1>")
